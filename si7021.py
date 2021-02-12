7dbe783be2d47779330ab213595fe4b54b21c618
Henric Linden <henriclinden@users.noreply.github.com>
2021-02-12 12:57:38 +0100

    Added SI7021 example

---
+++ si7021.py
@@ -0,0 +1,23 @@
+#
+# SI7021
+#
+
+import board
+import digitalio
+import busio
+import time
+import adafruit_si7021
+
+# Ensure automatic deinit
+with digitalio.DigitalInOut(board.SENSOR_POWER_ENABLE) as en:
+    en.direction = digitalio.Direction.OUTPUT
+    en.value = True
+    time.sleep(0.1)  # Important: sleep to let power stabilize
+
+    with busio.I2C(board.SCL, board.SDA, frequency=100000) as i2c:
+            si7021 = adafruit_si7021.SI7021(i2c)
+
+            while True:
+                print("Temperature: %0.1f degC" % si7021.temperature)
+                print("Humidity   : %0.1f %%" % si7021.relative_humidity)
+                time.sleep(2)
