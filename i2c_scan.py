c88cd064112bb1673a8001d483d00efa07c9adc9
Henric Linden <henriclinden@users.noreply.github.com>
2021-02-12 10:47:36 +0100

    Added I2C scan example

---
+++ i2c_scan.py
@@ -0,0 +1,19 @@
+#
+# Find all attached I2C devices
+#
+
+import board
+import digitalio
+import busio
+import time
+
+# Ensure automatic deinit
+with digitalio.DigitalInOut(board.SENSOR_POWER_ENABLE) as en:
+    en.direction = digitalio.Direction.OUTPUT
+    en.value = True
+    time.sleep(0.1) # Important: sleep to let power stabilize
+
+    with busio.I2C(board.SCL, board.SDA, frequency=100000) as i2c:
+        i2c.try_lock()
+        print(list(map(hex, i2c.scan())))
+        i2c.unlock()
